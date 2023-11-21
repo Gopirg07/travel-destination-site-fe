10eeb39af66837dcce13c46f55859e5640e6a847
Gopirg07 <gopinathofficial16@gmail.com>
2023-11-21 14:40:06 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Signin from './Components/Signin';
+import Add from './Components/Add';
+import Home from './Components/Home';
+import View from './Components/View';
+import { Updated } from './Components/Update';
+
+export const url="https://www.melivecode.com"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <Routes>
+      <Route path="/signin" element={<Signin/>}/>
+      <Route path="/add" element={<Add/>}/>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/view/:idx" element={<View/>}/>
+      <Route path="/update/:idx" element={<Updated/>}/> 
+     </Routes>
+
     </div>
   );
 }
